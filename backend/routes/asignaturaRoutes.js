7d1bdbd5707938ebfc2260b45b9fb73b61acfb90
Juan Carlos De León Amores <62060046+KenjiKom@users.noreply.github.com>
2025-02-25 20:18:42 +0100

    Visualizacion de predicciones: ahora categorizado por asignaturas.

--- backend/routes/asignaturaRoutes.js
+++ backend/routes/asignaturaRoutes.js
@@ -2,12 +2,36 @@ const express = require('express');
 const db = require('../db/connection');
 const router = express.Router();
 
-// Obtener todas las asignaturas
-router.get('/', (req, res) => {
-    db.query('SELECT * FROM Asignatura', (err, results) => {
+router.get('/asignaturas-profesor', async (req, res) => {
+    const { profesorId } = req.query;
+    const profesorIdInt = parseInt(profesorId, 10);
+    db.query(`
+            SELECT DISTINCT A.Nombre 
+            FROM TFG.Asignatura A
+            JOIN TFG.Matricula M ON A.id = M.id_asignatura
+            JOIN TFG.Usuario U ON M.id_usuario = U.id
+            WHERE U.id = ?;
+            `, [profesorIdInt], (err, results) => {
         if (err) return res.status(500).json({ error: err.message });
         res.json(results);
     });
 });
+
+
+router.get('/cursos', async (req, res) => {
+    const { profesorId, asignatura } = req.query;
+    const profesorIdInt = parseInt(profesorId, 10);
+    db.query(`
+        SELECT DISTINCT M.Curso
+            FROM TFG.Matricula M
+            JOIN TFG.Asignatura A ON M.id_asignatura = A.id
+            JOIN TFG.Usuario U ON M.id_usuario = U.id
+            WHERE U.id = ? AND A.Nombre = ?;
+        `, [profesorIdInt, asignatura], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+    });
+});
+
 
 module.exports = router;
